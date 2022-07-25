694c61d35da277ca160c2e22f7b06fcfb00ea9d6
weiquanyin <mihawben>
2022-07-25 15:18:13 +0800

    完成

--- model/Musiclink.go
+++ model/Musiclink.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	
 	"music/util"
 	"strings"
 
@@ -20,36 +21,35 @@ type Musiclinkout struct {
 
 //返回歌单及链接
 
-func Musiclinklist(page string,pagesize string, name string)([]Musiclinkout,int){
+func Musiclinklist(page string, pagesize string, name string) ([]Musiclinkout, int) {
 
-var MusiclikList []MusicLink
-var Kuwomodellist []util.Kuwomodel
-var Musiclinkoutlist []Musiclinkout
-var pageCount int
-err=db.Select("Linkname","Link").Find(&MusiclikList).Error
-for _,s:=range MusiclikList{
-if s.Linkname=="酷狗" {
-	var linke= s.Link
-	var build strings.Builder
-	build.WriteString(linke)
-	build.WriteString("?format=json&keyword=")
-	build.WriteString(name)
-	build.WriteString("&page=")
-	build.WriteString(page)
-	build.WriteString("&pagesize=")
-	build.WriteString(pagesize)
-	s3 := build.String()
-	Kuwomodellist= util.Kuwomusic(s3)
-	for ids,v:=range Kuwomodellist{
-	
-		Musiclinkoutlist[ids].MusicName=v.Filename
-		Musiclinkoutlist[ids].MusicLink=v.Sqhash
-		
+	var MusiclikList []MusicLink
+	var Kuwomodellist []util.Kuwomodel
+	var Musiclinkoutlist []Musiclinkout
+	var pageCount int
+	err = db.Select("Linkname", "Link").Find(&MusiclikList).Error
+	for _, s := range MusiclikList {
+		if s.Linkname == "酷狗" {
+			var linke = s.Link
+			var build strings.Builder
+			build.WriteString(linke)
+			build.WriteString("?format=json&keyword=")
+			build.WriteString(name)
+			build.WriteString("&page=")
+			build.WriteString(page)
+			build.WriteString("&pagesize=")
+			build.WriteString(pagesize)
+			s3 := build.String()
+			Kuwomodellist = util.Kuwomusic(s3)
+			for ids, v := range Kuwomodellist {
+				
+				Musiclinkoutlist[ids].MusicName = v.Data
+				Musiclinkoutlist[ids].MusicLink = v.Data
+
+			}
+			return Musiclinkoutlist, pageCount
+		}
 
 	}
-return Musiclinkoutlist,pageCount
-}
-
-}
-return Musiclinkoutlist,pageCount
+	return Musiclinkoutlist, pageCount
 }
--- util/musicpoat.go
+++ util/musicpoat.go
@@ -12,9 +12,12 @@ import (
 )
 
 type Kuwomodel struct {
-	Filename string `json:"filename"`
-	Sqhash   string `json:"sqhash"`
-	Key      string `json:"key"`
+	//Filename string `json:"filename"`
+	//Sqhash   string `json:"sqhash"`
+	//Key      string `json:"key"`
+	Status  float64     `json:"status"`
+	Errcode float64     `json:"errcode"`
+	Data    string `json:"data"`
 }
 
 //调用接口 返回音乐链接
@@ -23,14 +26,14 @@ func Kuwomusic(urlname string) []Kuwomodel {
 	client := &http.Client{}
 	reqest, err := http.NewRequest("GET", urlname, nil)
 	if err != nil {
-        fmt.Println(err)
-        log.Fatal(err)
-    }
+		fmt.Println(err)
+		log.Fatal(err)
+	}
 	var Kuwomodellist []Kuwomodel
 	reqest.Header.Set("Accept", "*/*")
-	//reqest.Header.Set("Accept-Charset", "GBK,utf-8;q=0.7,*;q=0.3")
-	reqest.Header.Set("Accept-Encoding", "gzip, deflate, br")
-	//reqest.Header.Set("Accept-Language", "zh-CN,zh;q=0.8")
+	reqest.Header.Set("Accept-Charset", "GBK,utf-8;q=0.7,*;q=0.3")
+	reqest.Header.Set("Accept-Encoding", "requests+bs4.BeautifulSoup")
+	reqest.Header.Set("Accept-Language", "zh-CN,zh;q=0.8")
 	reqest.Header.Set("Cache-Control", "no-cache")
 	reqest.Header.Set("Connection", "keep-alive")
 	reqest.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36 Edg/103.0.1264.62")
@@ -39,11 +42,12 @@ func Kuwomusic(urlname string) []Kuwomodel {
 	bodystr := ""
 	if response.StatusCode == 200 {
 
-		returnMap, _ := ParseResponse(response)
-
 		body, _ := ioutil.ReadAll(response.Body)
 		bodystr = string(body)
+
+		log.Printf("调用成功 数据为%s 读取完成/n", bodystr)
 		fmt.Println(bodystr)
+		returnMap := ParseResponse(body)
 		err := mapstructure.Decode(returnMap, &Kuwomodellist)
 		if err == nil {
 			return Kuwomodellist
@@ -56,12 +60,10 @@ func Kuwomusic(urlname string) []Kuwomodel {
 }
 
 //返回值 map处理
-func ParseResponse(response *http.Response) (map[string]interface{}, error) {
+func ParseResponse(Body []byte) map[string]interface{} {
 	var result map[string]interface{}
-	body, err := ioutil.ReadAll(response.Body)
-	if err == nil {
-		err = json.Unmarshal(body, &result)
-	}
 
-	return result, err
+	_ = json.Unmarshal(Body, &result)
+
+	return result
 }
